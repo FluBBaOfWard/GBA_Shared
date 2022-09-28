3d752a5198ac5342eac0df40785e82f289d6fa5a
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2022-09-28 14:56:36 +0200

    Added findRom(n) to header so it can be used.

--- FileHelper.h
+++ FileHelper.h
@@ -18,6 +18,8 @@ void drawSpinner(void);
 
 const RomHeader *browseForFile(void);
 
+const RomHeader *findRom(int n);
+
 /**
  * Returns the name of the rom as the given position.
  * @param  pos: Position of the rom in the list.
